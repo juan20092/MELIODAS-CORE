3b79ff4b6feb9073db2daf71d8d2155ecfbaec24
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-05-25 01:26:38 -0500

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,5 +1,11 @@
 let handler = async (m, { conn }) => {
 
+  const chatMetadata = await conn.groupMetadata(m.chat)
+  const groupAdmins = chatMetadata.participants.filter(p => p.admin)
+  const isAdmin = groupAdmins.some(admin => admin.id === m.sender)
+
+  if (!isAdmin) return
+
   const target =
     m.mentionedJid?.[0] ||
     m.quoted?.sender
@@ -11,11 +17,19 @@ let handler = async (m, { conn }) => {
                 𝐊𝐈𝐂𝐊
 ┗━━━━━━━━━┛
 
-➥ 𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙖 ο 𝙧𝙚𝙨𝙥𝙤𝙣𝙙𝙚
-➥ 𝙖𝙡 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙦𝙪𝙚
+➥ 𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙖 ο 𝙧𝙚𝙨𝙥ο𝙣𝙙𝙚
+➥ 𝙖𝙡 𝙪𝙨𝙪𝙖𝙧ἰο 𝙦𝙪𝙚
 ➥ 𝙙𝙚𝙨𝙚𝙖𝙨 𝙚𝙭𝙪𝙡𝙨𝙖𝙧
       `.trim(),
-      // Usamos global.rcanal completo como querías
+      ...global.rcanal
+    }, { quoted: m })
+  }
+
+  const groupOwner = chatMetadata.owner || chatMetadata.id.split('-')[0] + '@s.whatsapp.net'
+
+  if (target === groupOwner) {
+    return conn.sendMessage(m.chat, {
+      text: `❌ *No puedes expulsar al Dueño/Creador del grupo.*`,
       ...global.rcanal
     }, { quoted: m })
   }
@@ -41,7 +55,6 @@ let handler = async (m, { conn }) => {
 > ⟢ 𝙁𝙪𝙚 𝙚𝙭𝙪𝙡𝙨𝙖𝙙ο 𝙙𝙚𝙡 𝙜𝙧𝙪𝙥ο
       `.trim(),
       mentions: [target],
-      // 🚀 Fusionamos tu global.rcanal pero inyectando el target en las menciones para que funcione el enlace azul
       contextInfo: {
         ...(global.rcanal?.contextInfo || {}),
         mentionedJid: [target]
@@ -57,8 +70,8 @@ let handler = async (m, { conn }) => {
                 𝐊𝐈𝐂𝐊
 ┗━━━━━━━━━┛
 
-> ➥ 𝙀𝙡 𝙗ο𝙩 𝙣ο 𝙚𝙨 𝙖𝙙𝙢𝙞𝙣
-> ➥ 𝙊 𝙚𝙡 𝙪𝙨𝙪𝙖𝙧𝙞ο 𝙮𝙖 𝙣ο 𝙚𝙨𝙩𝙖
+> ➥ 𝙀𝙡 𝙗ο𝙩 𝙣ο 𝙚𝙨 𝙖𝙙𝙢𝙞н
+> ➥ 𝙊 𝙚𝙡 𝙪𝙨𝙪𝙖𝙧ἰο 𝙮𝙖 𝙣ο 𝙚𝙨𝙩𝙖
       `.trim(),
       ...global.rcanal
     }, { quoted: m })
@@ -71,5 +84,4 @@ handler.command = /^kick$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
-
 export default handler
