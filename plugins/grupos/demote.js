d63f6147da1e3653201c122504a7045410847d8a
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-06-30 11:58:31 -0500

    Actualizar demote.js

--- plugins/grupos/demote.js
+++ plugins/grupos/demote.js
@@ -43,18 +43,12 @@ let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
     console.error("Error al crear el Fake Chat:", err)
   }
 
-  // --- DISEÑO: SI FALTA EL USUARIO OBJETIVO ---
   if (!target) {
-    const textFalta = `*『 ACCIÓN REQUERIDA 』* \n\n` +
-      
-                      `> *responde a un mensaje o menciona a un @user que desees remover del cargo de Administrador* 🛡️`
-
     return conn.sendMessage(
       m.chat,
       {
-        text: textFalta,
+        text: '> *Menciona o responde al admin que deseas quitar* 👑🤪',
         contextInfo: {
-          mentionedJid: [m.sender], // Te menciona a ti indicando el error
           forwardingScore: 1,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
@@ -67,34 +61,25 @@ let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
     )
   }
 
-  // Ejecutar la degradación
   await conn.groupParticipantsUpdate(
     m.chat,
     [target],
     'demote'
   )
 
-  // Reacción de éxito
   await conn.sendMessage(m.chat, {
     react: {
-      text: '⚡',
+      text: '✅',
       key: m.key
     }
   })
 
-  // --- DISEÑO: MENSAJE DE ÉXITO ---
-  const textExito = `👑 *『 CONTROL DE ADMINISTRADORES 』* 👑\n\n` +
-                    `⚠️ *Aviso:* Un administrador ha sido removido.\n` +
-                    `👤 *Usuario:* @${target.split('@')[0]}\n` +
-                    `📉 *Acción:* Degradado a miembro común.\n\n` +
-                    `> ⚖️ _Acción ejecutada por el sistema de moderación._`
-
   await conn.sendMessage(
     m.chat,
     {
-      text: textExito,
+      text: `> *@${target.split('@')[0]} Ya no pertenece al grupo de admins* 😈`,
       contextInfo: {
-        mentionedJid: [target], // Mención al usuario degradado
+        mentionedJid: [target], 
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
