b07f4ecf7b2a6db0345c7f5d789954a74bb8543c
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-07-02 01:37:54 -0500

    Actualizar fantasmas.js

--- plugins/grupos/fantasmas.js
+++ plugins/grupos/fantasmas.js
@@ -1,43 +1,55 @@
 let handler = async (m, { conn, text, participants }) => {
-let member = participants.map(u => u.id)
-if (!text) {
-var sum = member.length
-} else {
-var sum = Number(text)
-}
-var total = 0
-var sider = []
+  let member = participants.map(u => u.id)
 
-for (let i = 0; i < sum && i < member.length; i++) {
-let users = m.isGroup ? participants.find(u => u.id == member[i]) || {} : {}
+  let sum = !text ? member.length : Number(text)
+  let total = 0
+  let sider = []
 
-let user = global.db?.data?.users?.[member[i]]
+  for (let i = 0; i < sum && i < member.length; i++) {
+    let users = m.isGroup ? participants.find(u => u.id == member[i]) || {} : {}
 
-if ((typeof user == 'undefined' || user.chat == 0) && !users.isAdmin && !users.isSuperAdmin) {
-if (typeof user !== 'undefined') {
-if (user.whitelist == false) {
-total++
-sider.push(member[i])
-}
-} else {
-total++
-sider.push(member[i])
-}
-}
-}
+    let user = global.db?.data?.users?.[member[i]]
 
-if (total == 0) return conn.reply(m.chat, `*[❗𝙸𝙽𝙵𝙾❗]* 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙵𝙰𝙽𝚃𝙰𝚂𝙼𝙰𝚂, 𝚀𝚄𝙴 𝙱𝚄𝙴𝙽 𝚃𝚁𝙰𝙱𝙰𝙹𝙾 𝙷𝙰𝙲𝙴 𝙴𝙻 𝙰𝙳𝙼𝙸𝙽`, m)
+    if ((!user || user.chat == 0) && !users.isAdmin && !users.isSuperAdmin) {
+      if (user) {
+        if (user.whitelist == false) {
+          total++
+          sider.push(member[i])
+        }
+      } else {
+        total++
+        sider.push(member[i])
+      }
+    }
+  }
 
-m.reply(`[ ⚠ 𝚁𝙴𝚅𝙸𝚂𝙸Ó𝙽 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙰 ⚠ ]
+  if (total == 0) {
+    return conn.reply(
+      m.chat,
+      `*[❗ INFO ❗]* ESTE GRUPO NO TIENE FANTASMAS, ¡BUEN TRABAJO!`,
+      m
+    )
+  }
 
-𝙶𝚁𝚄𝙿𝙾: ${await conn.getName(m.chat)}
-𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂: ${sum}
+  let teks = `[ ⚠ REVISIÓN INACTIVA ⚠ ]
 
-[ ⇲ 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙵𝙰𝙽𝚃𝙰𝚂𝙼𝙰𝚂 ⇱ ]
+👥 *GRUPO:* ${await conn.getName(m.chat)}
+👤 *MIEMBROS:* ${sum}
 
-${sider.map(v => '👻 @' + v.replace(/@.+/, '')).join('\n')}
+[ 👻 LISTA DE FANTASMAS 👻 ]
 
-𝙽𝙾𝚃𝙰: 𝙴𝚂𝚃𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 100% 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾, 𝙴𝙻 𝙱𝙾𝚃 𝙸𝙽𝙸𝙲𝙸𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙴𝙾 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙳𝙴𝚂𝙳𝙴 𝙻𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝙲𝙸Ó𝙽 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾`, null, { mentions: sider })
+${sider.map(v => `👻 @${v.split('@')[0]}`).join('\n')}
+
+*NOTA:* Esto no puede ser 100% correcto, el bot inicia el conteo de mensajes desde que fue agregado al grupo.`
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      text: teks,
+      mentions: sider
+    },
+    { quoted: m }
+  )
 }
 
 handler.help = ['fantasmas']
@@ -45,5 +57,6 @@ handler.tags = ['group']
 handler.command = /^(verfantasmas|fantasmas|sider)$/i
 handler.admin = true
 handler.botAdmin = true
+handler.group = true
 
 export default handler
